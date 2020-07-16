089b3a50320dea0518eb3c9f10ac277718f6bed8
Lenovo <Lenovo@Lenovo-PC>
2020-07-16 08:35:39 +0530

    Adding the Project into my GIT

---
+++ NareshOnlineTraining/src/Addition.java
@@ -0,0 +1,20 @@
+
+public class Addition {
+
+	int sum;
+	
+	void addition(int num1,int num2){
+		sum=num1+num2;
+		System.out.println(sum);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Addition add=new Addition();
+		add.addition(100,500);
+		add.addition(200,400);
+
+	}
+
+}
---
+++ NareshOnlineTraining/src/WelcomeJava.java
@@ -0,0 +1,47 @@
+
+public class WelcomeJava {
+	//static variable
+	static String instituename="Naresh";
+	//instance variables
+    static  int a;
+	
+	String name;
+	//non static method
+	
+	
+	  void display(){
+            System.out.println("I am in display method");
+            System.out.println(instituename);
+	  }
+	  //static method
+	  static void input(){
+		  WelcomeJava	std1=new WelcomeJava();
+		  System.out.println(std1.instituename);  
+	  }
+	
+    	public static void main(String[] args){
+    		//syntax for object creation
+    		WelcomeJava	std1=new WelcomeJava();
+    		
+    		std1.name="arun";
+    		System.out.println("Student "+std1.name+" is learining selenium from"+WelcomeJava.instituename);
+    		
+    		WelcomeJava std2=new WelcomeJava();
+    		std2.name="kumar";
+    		System.out.println("Student "+std2.name+" is learining selenium from"+instituename);
+    		
+    		
+    		std1.a=200;
+    		System.out.println(std1.a);
+    		std2.a=400;    		
+    		System.out.println(std1.a);
+    		
+    		
+    		
+    		
+    	}
+    	
+    	
+    	
+
+}
